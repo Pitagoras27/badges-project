a58d84d4510649b4489417e5af190849143545b8
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-04 22:27:00 +0200

    Estructura básica, dependencias en package json, simulacion DB

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, Platzi Badges!</h1>;
+
+const container = document.getElementById('app');
+
+ReactDOM.render(element, container);
