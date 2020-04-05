03c979805d4ae89b0d487d708f80428ef8141408
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-05 04:32:03 +0200

    React fragment, implementación de component layout y props children, utilizacion de Link de react router dom. Creacion de componente home

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import BadgeNew from '../pages/BadgeNew';
+import Layout from './Layout';
+import Home from '../pages/Home';
 import Badges from '../pages/Badges';
+import BadgeNew from '../pages/BadgeNew';
+import NotFound from '../pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/badges" component={Badges} />
-        <Route exact path="/badges/new" component={BadgeNew} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/badges" component={Badges} />
+          <Route exact path="/badges/new" component={BadgeNew} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
